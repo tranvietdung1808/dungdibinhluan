b7e6fe6000f66d033b75edd769108b5d67956020
tranvietdung1808 <dungba66@gmail.com>
2026-03-24 09:36:26 +0700

    fix: add force-dynamic to prevent caching on guides list

--- app/huong-dan/page.tsx
+++ app/huong-dan/page.tsx
@@ -3,6 +3,9 @@ import { resolveThumbnailSrc } from '@/utils/r2'
 import Link from 'next/link'
 import Image from 'next/image'
 
+export const dynamic = 'force-dynamic'
+export const revalidate = 0
+
 type Guide = {
   id: string
   title: string
